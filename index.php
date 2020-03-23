18d05e969289c5c39c14dbc80aaa23eab6570496
Francisco Filho <franciscoguinarte@yahoo.com.br>
2020-03-23 11:49:29 -0300

    "Commit Novo Front teste1" -q

--- index.php
+++ index.php
@@ -1,59 +1,61 @@
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Login - PHP + MySQL - Canal TI</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-    <link rel="stylesheet" href="css/bulma.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/login.css">
-</head>
+<!DOCTYPE html5>
+<html lang="pt-BR">
+  <head>
 
-<body>
-    <section class="hero is-success is-fullheight">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Sistema de Login Simples PHP + MySQL</h3>
-                    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <meta name="keywords" content="PHP, SQL, MySQL, Cadastro">
+    <meta name="description" content="Criado por: Francisco Filho">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Sistema de Login PHP + MySQL</title>
 
-                    <?php 
-                    if(isset($_SESSION['nao_autenticado'])):
-                    ?>
-                    
-                    <div class="notification is-danger">
-                      <p>ERRO: Usuário ou senha inválidos.</p>
-                    </div>
-                    
-                    <?php
-                    session_destroy();
-                    endif;
-                    
-                    ?>
-                    <div class="box">
-                        <form action="login.php" method="POST">
-                            <div class="field">
-                                <div class="control">
-                                    <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
-                                </div>
-                            </div>
+  </head>
+  <!-- <style>
+    @media only screen and (max-width: 600px) {
+      #container-mobile{
+        display:none;
+      }
+    }
+    
+  </style> -->
+  <style>
+    .container {
+   
+    padding-right: 25px;
+    padding-left: 25px;
+ 
+ 
 
-                            <div class="field">
-                                <div class="control">
-                                    <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
-                                </div>
-                            </div>
-                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Entrar</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</body>
+    }
+  </style>
+  <body>
+
+    <header class = "jumbotron text-center text-dark bg-info">
+      <h1>Sistema de Login </h1>
+    </header>
+    <div class = "container text-center border w-50 h-50 center" name = "container-mobile">
+      <form action="login.php" method="POST">
+          <div class = "form-group text-left ">
+            <!-- <label for="login" >Usuario</label>  -->
+            <input name="usuario" type="text" id = "usuario" class= "form-control container" placeholder="Seu usuário">
+          </div>
+          <div class = "form-group text-left ">
+            <!-- <label for = "senha">Senha</label> -->
+            <input name="senha" id ="senha" type="password" class = "form-control container" placeholder="Sua senha">
+          </div>
+          <div class = "form-group text-right" >
+            <input type="submit" class = "btn btn-primary" value="Login"> 
+          </div>
+          <div class = "form-group text-right">
+            <input type="button" class ="btn btn-secondary" value="Cadastrar">
+          </div>
+
+        </form>                         
+    </div>  
+
+
+
+</div>
+  </body>
 
 </html>
